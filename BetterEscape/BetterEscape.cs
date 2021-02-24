b9dfaa77fea2bb53c89afd7a2c993823f95efafa
4310V343k <denromanov2005@gmail.com>
2021-02-24 16:41:44 +0300

    bump

--- BetterEscape/BetterEscape.cs
+++ BetterEscape/BetterEscape.cs
@@ -14,7 +14,7 @@ namespace BetterEscape
 		public override string Author { get; } = "Remindme";
 		public override string Name { get; } = "Better Escape";
 		public override string Prefix { get; } = "bEscape";
-		public override Version Version { get; } = new Version(2, 0, 0);
+		public override Version Version { get; } = new Version(2, 0, 1);
 		public override Version RequiredExiledVersion { get; } = new Version(2, 1, 34);
 
 		private EventHandlers EventHandlers { get; set; }
