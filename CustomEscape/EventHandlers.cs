be70d10bf05e37c457dca319dd6f9b1793e6ce88
4310V343k <denromanov2005@gmail.com>
2021-04-10 21:16:12 +0300

    fuck

--- CustomEscape/EventHandlers.cs
+++ CustomEscape/EventHandlers.cs
@@ -15,7 +15,6 @@ namespace CustomEscape
             EscapePos.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f); // stop bumping into that shit. not 0 because unity
             EscapePos.transform.localPosition = new Vector3(170f, 985f, 26f);
             Log.Debug($"modified the sphere: {EscapePos.transform.localScale}, {EscapePos.transform.localPosition}", CustomEscape.Singleton.Config.Debug);
-            Component[] comps = EscapePos.GetComponents(typeof(Component));
 
             SphereCollider collider = EscapePos.GetComponent<SphereCollider>();
             Log.Debug($"got a collider: {collider}", CustomEscape.Singleton.Config.Debug);
